447e542c627e129531142bdf07f772ee07dfbe77
Alex <smigymuller@gmail.com>
2024-03-05 18:43:43 -0500

    fixing chrome issues

--- app.py
+++ app.py
@@ -5,19 +5,13 @@ app = Flask(__name__)
 
 config = {
     'apiKey': "AIzaSyD3Ax_l85vk3U5P5h2hAEIWdASguZAHcXs",
-
     'authDomain': "csi2999team10-f457b.firebaseapp.com",
-
     'projectId': "csi2999team10-f457b",
-
     'storageBucket': "csi2999team10-f457b.appspot.com",
-
     'messagingSenderId': "639473870266",
-
     'appId': "1:639473870266:web:a9a97e2ffe8c548f63e983",
     'measurementId': "G-7NCQG5M9FB",
     'databaseURL': "",
-
 }
 
 firebase = pyrebase.initialize_app(config)
